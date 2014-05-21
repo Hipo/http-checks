fe7457f9361b39e5ee6fef67db7330d2fe38aaf5
ybrs <aybars.badur@gmail.com>
2014-05-21 14:14:13 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='http-checks',
-    version='0.1',
+    version='0.1.1',
     long_description=__doc__,
     packages=['httpchecks'],
     include_package_data=True,

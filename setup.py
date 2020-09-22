24cea7fab286db84e6c058f6c791d9581d385c89
Ali <alicertel@gmail.com>
2020-09-22 15:31:38 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='http-checks',
-    version='0.2.0',
+    version='0.2.1',
     author='ybrs',
     description='http-checks is an application that can test a couple of hundred urls in seconds',
     long_description=__doc__,
